99b85cb9dd9c8a809be398f6a0628bf828a437a1
Maximo Lopez <7888441+mxlopez25@users.noreply.github.com>
2023-06-24 19:15:38 -0400

    Initial Commit

---
+++ app/src/main/java/com/maloac/pokedexproject/network/NetworkLayer.kt
@@ -0,0 +1,18 @@
+package com.maloac.pokedexproject.network
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+object NetworkLayer {
+    private val BASE_URL = "https://pokeapi.co/api/v2/"
+
+    val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+
+    val retrofit: Retrofit = Retrofit
+        .Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
+        .build()
+}
---
+++ app/src/main/java/com/maloac/pokedexproject/viewmodel/MainViewModel.kt
@@ -0,0 +1,6 @@
+package com.maloac.pokedexproject.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+}
