82f852a206bdba9ab90698dc936629c1517ebe08
Maximo Lopez <7888441+mxlopez25@users.noreply.github.com>
2023-06-25 03:59:37 -0400

    PokedexListAdapter updated to open DetailPage

--- app/src/main/java/com/maloac/pokedexproject/adapters/PokedexListAdapter.kt
+++ app/src/main/java/com/maloac/pokedexproject/adapters/PokedexListAdapter.kt
@@ -1,5 +1,7 @@
 package com.maloac.pokedexproject.adapters
 
+import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -7,11 +9,16 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.maloac.pokedexproject.R
 import com.maloac.pokedexproject.models.ResultsItem
+import com.maloac.pokedexproject.screen.DetailActivity
+import java.util.*
 
 class PokedexListAdapter (private val dataSet: List<ResultsItem>) :
 RecyclerView.Adapter<PokedexListAdapter.ViewHolder>()
 {
+    private var context: Context? = null
+
     class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
+
         val tvPokemonName: TextView
 
         init {
@@ -28,7 +35,20 @@ RecyclerView.Adapter<PokedexListAdapter.ViewHolder>()
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.tvPokemonName.text = dataSet[position].name
+        holder.tvPokemonName.text = dataSet[position].name?.replaceFirstChar {
+            if (it.isLowerCase()) it.titlecase(
+                Locale.getDefault()
+            ) else it.toString()
+        }
+
+        holder.itemView.setOnClickListener { it ->
+            val context = it.context
+            val intent = Intent(context, DetailActivity::class.java)
+            intent.putExtra("URL", dataSet[position].url)
+            intent.putExtra("INDEX", position.toString())
+
+            context.startActivity(intent)
+        }
     }
 
     override fun getItemCount(): Int {
