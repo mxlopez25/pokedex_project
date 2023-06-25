662a5c4cea66ee7a4d9e6eaac30839717bfcf826
Maximo Lopez <7888441+mxlopez25@users.noreply.github.com>
2023-06-25 14:23:15 -0400

    Implementation of activity starting added

--- app/src/main/java/com/maloac/pokedexproject/screen/DetailActivity.kt
+++ app/src/main/java/com/maloac/pokedexproject/screen/DetailActivity.kt
@@ -2,8 +2,17 @@ package com.maloac.pokedexproject.screen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.maloac.pokedexproject.R
 import com.maloac.pokedexproject.databinding.ActivityDetailBinding
+import com.maloac.pokedexproject.helpers.PokedexContants
+import com.maloac.pokedexproject.viewmodel.MainViewModel
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 class DetailActivity : AppCompatActivity() {
     private var _binding: ActivityDetailBinding? = null
@@ -11,6 +20,9 @@ class DetailActivity : AppCompatActivity() {
 
     private var url: String? = null
     private var index: String? = null
+    private var name: String? = null
+
+    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,10 +30,39 @@ class DetailActivity : AppCompatActivity() {
         _binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        url = intent.getStringExtra("URL")
-        index = intent.getStringExtra("INDEX")
+        url = intent.getStringExtra(PokedexContants.URL_TAG)
+        index = intent.getStringExtra(PokedexContants.INDEX_TAG)
+        name = intent.getStringExtra(PokedexContants.NAME_TAG)
 
+        name?.let { viewModel.getPokemonByName(it) }
 
+        lifecycleScope.launch {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.CREATED) {
+                launch {
+                    viewModel.pokedexDetail.collect {
+                        data ->
+                        Log.d("DataReceived", "$data")
+                    }
+                }
+
+                launch {
+                    viewModel.requestState.collect {
+                        state ->
+                        when(state) {
+                            is MainViewModel.RequestState.InProcess -> {
+
+                            }
+                            is MainViewModel.RequestState.Success -> {
+
+                            }
+                            is MainViewModel.RequestState.Error -> {
+
+                            } else -> Unit
+                        }
+                    }
+                }
+            }
+        }
 
     }
 }
