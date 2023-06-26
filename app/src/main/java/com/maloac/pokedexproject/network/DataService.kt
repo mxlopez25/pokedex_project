fe0f6093dd30cee89ecee71ee77746d694446c08
Maximo Lopez <7888441+mxlopez25@users.noreply.github.com>
2023-06-26 15:46:36 -0400

    Added parameters and defaults to getAllPokemons request

--- app/src/main/java/com/maloac/pokedexproject/network/DataService.kt
+++ app/src/main/java/com/maloac/pokedexproject/network/DataService.kt
@@ -5,11 +5,17 @@ import com.maloac.pokedexproject.models.PokedexDataResponse
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface DataService {
     @GET("pokemon")
-    suspend fun getAllPokemons(): Response<PokedexDataResponse>
+    suspend fun getAllPokemons(
+        @Query("offset")offset: String = "0",
+        @Query("limit")limit: String = "151"
+    ): Response<PokedexDataResponse>
 
     @GET("pokemon/{name}")
     suspend fun getPokemonByName(@Path("name")path: String): Response<PokedexData>
+
+
 }
